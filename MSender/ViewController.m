f1513e04b037ec3c012dd61c5d97788a4f29f8b7
MartinRGB <martinrgb@DDQdeiMac.local>
2015-06-07 17:07:26 +0800

    Add pop animation(overshoot) to scrollview

--- MSender/ViewController.m
+++ MSender/ViewController.m
@@ -49,8 +49,8 @@
 @property (strong, nonatomic) FilteredImageView *t5v;
 
 
-@property(nonatomic) BOOL bounces;
-
+@property (nonatomic) CGFloat startValue;
+@property (nonatomic) CGFloat popAnimationProgress;
 
 @end
 
@@ -63,14 +63,159 @@
     _scview.delegate = self;
     // Do any additional setup after loading the view, typically from a nib.
 
+    //Add Tree & Filter
+    [self TreeImagenFilter];
+   
+}
 
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    
+    if (scrollView == self.scview ){
+    
+        [self parallax];
+      
+    }
+}
+
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    CGFloat scrolly = self.scview.contentOffset.y;
+    if (scrolly < -100.00) {
+
+
+        [self Paperplane];
+        [self ListPOP];
+        
+      
+        
+        }
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
+{
+    
+    _LNew.alpha = 0;
+    
+
+}
+
+-(void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView{
+     if (scrollView.contentOffset.y < 0) {
+        [scrollView setContentOffset:scrollView.contentOffset animated:NO];
+        
+        self.startValue = self.scview.contentOffset.y;
+        [self popAnimation];
+        
+     }
+    
+    
+}
+
+//Define popAnimation(scrollview bounce animation),use the POP to animate any property.(Written by my teacher Alienjunx)
+
+- (void)popAnimation {
+    _popAnimationProgress = 0;
+    POPSpringAnimation *animation = [self pop_animationForKey:@"popAnimation"];
+    
+    if (!animation) {
+        animation = [POPSpringAnimation animation];
+        animation.springBounciness = 15;
+        animation.springSpeed = 8;
+        animation.completionBlock = ^(POPAnimation *anim, BOOL finished){
+            
+            [self.scview setContentOffset:CGPointMake(0, 0)];
+            [self pop_removeAnimationForKey:@"popAnimation"];
+        };
+        animation.property = [POPAnimatableProperty propertyWithName:@"popAnimationProgress" initializer:^(POPMutableAnimatableProperty *prop) {
+            prop.readBlock = ^(ViewController *obj, CGFloat values[]) {
+                values[0] = obj.popAnimationProgress;
+            };
+            prop.writeBlock = ^(ViewController *obj, const CGFloat values[]) {
+                obj.popAnimationProgress = values[0];
+            };
+            prop.threshold = 0.001;
+        }];
+        
+        [self pop_addAnimation:animation forKey:@"popAnimation"];
+    }
+    animation.toValue =@(1.0);
+}
+
+//Define progress
+
+- (void)setPopAnimationProgress:(CGFloat)progress {
+    _popAnimationProgress = progress;
+    
+    CGFloat transition = POPTransition(progress, self.startValue, 0);
+    [self.scview setContentOffset:CGPointMake(0, transition)];
+    
+}
+
+static inline CGFloat POPTransition(CGFloat progress, CGFloat startValue, CGFloat endValue) {
+    return startValue + (progress * (endValue - startValue));
+}
+
+//parallax
+
+
+- (void) parallax{
+    //parallax
+    
+    CGFloat scrolly = self.scview.contentOffset.y;
+    NSLog(@"scrolly is %f",scrolly);
+    
+    [_mo1 setFrame:CGRectMake(0, 55-(scrolly/2), 320, 234)];
+    [_mo2 setFrame:CGRectMake(0, 74-(scrolly/3), 320, 242)];
+    [_mo3 setFrame:CGRectMake(0, 89-(scrolly/4), 320, 228)];
+    
+    
+    
+    //btn
+    CGFloat rotation = self.scview.contentOffset.y*4/568;
+    
+    _plane.transform = CGAffineTransformMakeRotation(M_PI * rotation  / 4.0);
+    _BBtn.transform = CGAffineTransformMakeTranslation(0, -rotation * 2);
+    
+    //tree filter
+    CGFloat rotation2 = self.scview.contentOffset.y*2/568;
+    CGFloat inputAngle = -rotation2+0.03;
+    CGFloat inputAngle2 = rotation2-0.03;
+    
+    //(use 'if' for bug fix)
+    if (inputAngle >=0.01 || inputAngle <=-0.01) {
+        [self.t1v.filter setValue:@(inputAngle) forKey:kCIInputAngleKey];
+        [self.t1v setNeedsDisplay];
+        [self.t2v.filter setValue:@(inputAngle) forKey:kCIInputAngleKey];
+        [self.t2v setNeedsDisplay];
+        [self.t3v.filter setValue:@(inputAngle2) forKey:kCIInputAngleKey];
+        [self.t3v setNeedsDisplay];
+        [self.t4v.filter setValue:@(inputAngle2) forKey:kCIInputAngleKey];
+        [self.t4v setNeedsDisplay];
+        [self.t5v.filter setValue:@(inputAngle2) forKey:kCIInputAngleKey];
+        [self.t5v setNeedsDisplay];
+    }
+    
+
+}
+
+//Add Tree Image to FilterImageView & Add Filter
+
+-(void) TreeImagenFilter{
+    //Define Tree Image
     self.t1 = [UIImage imageNamed:@"t1"];
     self.t2 = [UIImage imageNamed:@"t2"];
     self.t3 = [UIImage imageNamed:@"t3"];
     self.t4 = [UIImage imageNamed:@"t4"];
     self.t5 = [UIImage imageNamed:@"t5"];
     
-    //filter & filterimageview
+    //filter & filterimageview(Tree Image)
     self.filter =[CIFilter filterWithName:@"CITwirlDistortion"];
     [self.filter setValue:[CIVector vectorWithX:1 Y:100] forKey:kCIInputCenterKey];
     [self.filter setValue:@190 forKey:kCIInputRadiusKey];
@@ -100,7 +245,7 @@
     [self.t2v setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.t2v setNeedsDisplay];
     [self.mo2 addSubview:self.t2v];
-
+    
     self.t3v = [[FilteredImageView alloc] initWithFrame:CGRectMake(170, -73, 100, 144)];
     self.t3v.inputImage = self.t3;
     self.t3v.filter=self.filter2;
@@ -131,163 +276,102 @@
     [self.t5v setNeedsDisplay];
     [self.mo1 addSubview:self.t5v];
     
-   
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+//Define the paperplane's path
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
+-(void) Paperplane{
+    //PaintCode
+    _plane.transform = CGAffineTransformMakeRotation(M_PI * -0.704  / 4.0);
     
-    if (scrollView == self.scview && self.scview.contentOffset.y < 0){
+    UIBezierPath* bezierPath = UIBezierPath.bezierPath;
+    [bezierPath moveToPoint: CGPointMake(40.5, 165.5)];
+    [bezierPath addCurveToPoint: CGPointMake(383, -1.5) controlPoint1: CGPointMake(40.5, 165.5) controlPoint2: CGPointMake(285.25, 32.25)];
+    [bezierPath addCurveToPoint: CGPointMake(434.5, 32.5) controlPoint1: CGPointMake(480.75, -35.25) controlPoint2: CGPointMake(583.87, -9.75)];
+    [bezierPath addCurveToPoint: CGPointMake(-214.5, 167.5) controlPoint1: CGPointMake(262.5, 86.5) controlPoint2: CGPointMake(-120.5, 157.5)];
+    [bezierPath addCurveToPoint: CGPointMake(40.5, 165.5) controlPoint1: CGPointMake(-131.5, 167.5) controlPoint2: CGPointMake(40.5, 165.5)];
     
-        //parallax
-
-        CGFloat scrolly = self.scview.contentOffset.y;
-        NSLog(@"scrolly is %f",scrolly);
-
-        [_mo1 setFrame:CGRectMake(0, 55-(scrolly/2), 320, 234)];
-        [_mo2 setFrame:CGRectMake(0, 74-(scrolly/3), 320, 242)];
-        [_mo3 setFrame:CGRectMake(0, 89-(scrolly/4), 320, 228)];
-        
-        
-        
-        //btn
-        CGFloat rotation = self.scview.contentOffset.y*4/568;
-        CGFloat rotation2 = self.scview.contentOffset.y*2/568;
-        _plane.transform = CGAffineTransformMakeRotation(M_PI * rotation  / 4.0);
-        _BBtn.transform = CGAffineTransformMakeTranslation(0, -rotation * 2);
     
-        //tree filter(bug fix)
-        CGFloat inputAngle = -rotation2+0.03;
-        CGFloat inputAngle2 = rotation2-0.03;
- 
-        [self.t1v.filter setValue:@(inputAngle) forKey:kCIInputAngleKey];
-        [self.t1v setNeedsDisplay];
-        [self.t2v.filter setValue:@(inputAngle) forKey:kCIInputAngleKey];
-        [self.t2v setNeedsDisplay];
-        [self.t3v.filter setValue:@(inputAngle2) forKey:kCIInputAngleKey];
-        [self.t3v setNeedsDisplay];
-        [self.t4v.filter setValue:@(inputAngle2) forKey:kCIInputAngleKey];
-        [self.t4v setNeedsDisplay];
-        [self.t5v.filter setValue:@(inputAngle2) forKey:kCIInputAngleKey];
-        [self.t5v setNeedsDisplay];
-
-      
-    }
+    //Written by Alienjunx
+    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    animation.duration = 2.5;
+    animation.timingFunction = [CAMediaTimingFunction functionWithControlPoints:0.445 : 0.5 :0.55 :0.75];
+    animation.path = bezierPath.CGPath;
+    animation.rotationMode = kCAAnimationRotateAuto;
+    [self.plane.layer addAnimation:animation forKey:nil];
 }
 
+//Define the list's animation
 
-- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
-{
- self.scview.bounces = YES;
-    CGFloat scrolly = self.scview.contentOffset.y;
-    if (scrolly < -100.00) {
-
-
-        
-        //PaintCode
-        _plane.transform = CGAffineTransformMakeRotation(M_PI * -0.704  / 4.0);
-        
-        UIBezierPath* bezierPath = UIBezierPath.bezierPath;
-        [bezierPath moveToPoint: CGPointMake(40.5, 165.5)];
-        [bezierPath addCurveToPoint: CGPointMake(383, -1.5) controlPoint1: CGPointMake(40.5, 165.5) controlPoint2: CGPointMake(285.25, 32.25)];
-        [bezierPath addCurveToPoint: CGPointMake(434.5, 32.5) controlPoint1: CGPointMake(480.75, -35.25) controlPoint2: CGPointMake(583.87, -9.75)];
-        [bezierPath addCurveToPoint: CGPointMake(-214.5, 167.5) controlPoint1: CGPointMake(262.5, 86.5) controlPoint2: CGPointMake(-120.5, 157.5)];
-        [bezierPath addCurveToPoint: CGPointMake(40.5, 165.5) controlPoint1: CGPointMake(-131.5, 167.5) controlPoint2: CGPointMake(40.5, 165.5)];
-        
-        
-        //Written by Alienjunx
-        CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
-        animation.duration = 2.5;
-        animation.timingFunction = [CAMediaTimingFunction functionWithControlPoints:0.445 : 0.5 :0.55 :0.75];
-        animation.path = bezierPath.CGPath;
-        animation.rotationMode = kCAAnimationRotateAuto;
-        [self.plane.layer addAnimation:animation forKey:nil];
-        
-        
-        //list
-        CGPoint lnc=_LNew.center;
-        _LNew.center=lnc;
-        CGPoint l1c=_L1.center;
-        _L1.center=l1c;
-        CGPoint l2c=_L2.center;
-        _L2.center=l2c;
-        CGPoint l3c=_L3.center;
-        _L3.center=l3c;
-        CGPoint l4c=_L4.center;
-        _L4.center=l4c;
-        CGPoint l5c=_L5.center;
-        _L5.center=l5c;
-        CGPoint l6c=_L6.center;
-        _L6.center=l6c;
-        
-        [UIView animateWithDuration:0.5 delay:2.5 options: UIViewAnimationOptionCurveEaseInOut animations:^{
-            _LNew.alpha = 1;
-        } completion:nil];
-        
-        POPSpringAnimation * LNY = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
-        LNY.beginTime = CACurrentMediaTime() + 2.5;
-        LNY.toValue = @(lnc. y+= 62);
-        LNY.springBounciness = 10;
-        LNY.springSpeed = 10;
-        [_LNew pop_addAnimation:LNY forKey:@"LNY"];
-        
-        POPSpringAnimation * L1Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
-        L1Y.beginTime = CACurrentMediaTime() + 2.5;
-        L1Y.toValue = @(l1c. y+= 62);
-        L1Y.springBounciness = 10;
-        L1Y.springSpeed = 10;
-        [_L1 pop_addAnimation:L1Y forKey:@"L1Y"];
-        
-        POPSpringAnimation * L2Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
-        L2Y.beginTime = CACurrentMediaTime() + 2.5;
-        L2Y.toValue = @(l2c. y+= 62);
-        L2Y.springBounciness = 10;
-        L2Y.springSpeed = 10;
-        [_L2 pop_addAnimation:L2Y forKey:@"L2Y"];
-        
-        POPSpringAnimation * L3Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
-        L3Y.beginTime = CACurrentMediaTime() + 2.5;
-        L3Y.toValue = @(l3c. y+= 62);
-        L3Y.springBounciness = 10;
-        L3Y.springSpeed = 10;
-        [_L3 pop_addAnimation:L3Y forKey:@"L3Y"];
-        
-        POPSpringAnimation * L4Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
-        L4Y.beginTime = CACurrentMediaTime() + 2.5;
-        L4Y.toValue = @(l4c. y+= 62);
-        L4Y.springBounciness = 10;
-        L4Y.springSpeed = 10;
-        [_L4 pop_addAnimation:L4Y forKey:@"L4Y"];
-        
-        POPSpringAnimation * L5Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
-        L5Y.beginTime = CACurrentMediaTime() + 2.5;
-        L5Y.toValue = @(l5c. y+= 62);
-        L5Y.springBounciness = 10;
-        L5Y.springSpeed = 10;
-        [_L5 pop_addAnimation:L5Y forKey:@"L5Y"];
-        
-        POPSpringAnimation * L6Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
-        L6Y.beginTime = CACurrentMediaTime() + 2.5;
-        L6Y.toValue = @(l6c. y+= 62);
-        L6Y.springBounciness = 10;
-        L6Y.springSpeed = 10;
-        [_L6 pop_addAnimation:L6Y forKey:@"L6Y"];
-        
-        }
-}
-
-- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
-{
+-(void) ListPOP{
     
-    _LNew.alpha = 0;
-      [scrollView setContentOffset:scrollView.contentOffset animated:YES];
-
+    //list
+    CGPoint lnc=_LNew.center;
+    _LNew.center=lnc;
+    CGPoint l1c=_L1.center;
+    _L1.center=l1c;
+    CGPoint l2c=_L2.center;
+    _L2.center=l2c;
+    CGPoint l3c=_L3.center;
+    _L3.center=l3c;
+    CGPoint l4c=_L4.center;
+    _L4.center=l4c;
+    CGPoint l5c=_L5.center;
+    _L5.center=l5c;
+    CGPoint l6c=_L6.center;
+    _L6.center=l6c;
+    
+    [UIView animateWithDuration:0.5 delay:2.5 options: UIViewAnimationOptionCurveEaseInOut animations:^{
+        _LNew.alpha = 1;
+    } completion:nil];
+    
+    POPSpringAnimation * LNY = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
+    LNY.beginTime = CACurrentMediaTime() + 2.5;
+    LNY.toValue = @(lnc. y+= 62);
+    LNY.springBounciness = 10;
+    LNY.springSpeed = 10;
+    [_LNew pop_addAnimation:LNY forKey:@"LNY"];
+    
+    POPSpringAnimation * L1Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
+    L1Y.beginTime = CACurrentMediaTime() + 2.5;
+    L1Y.toValue = @(l1c. y+= 62);
+    L1Y.springBounciness = 10;
+    L1Y.springSpeed = 10;
+    [_L1 pop_addAnimation:L1Y forKey:@"L1Y"];
+    
+    POPSpringAnimation * L2Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
+    L2Y.beginTime = CACurrentMediaTime() + 2.5;
+    L2Y.toValue = @(l2c. y+= 62);
+    L2Y.springBounciness = 10;
+    L2Y.springSpeed = 10;
+    [_L2 pop_addAnimation:L2Y forKey:@"L2Y"];
+    
+    POPSpringAnimation * L3Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
+    L3Y.beginTime = CACurrentMediaTime() + 2.5;
+    L3Y.toValue = @(l3c. y+= 62);
+    L3Y.springBounciness = 10;
+    L3Y.springSpeed = 10;
+    [_L3 pop_addAnimation:L3Y forKey:@"L3Y"];
+    
+    POPSpringAnimation * L4Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
+    L4Y.beginTime = CACurrentMediaTime() + 2.5;
+    L4Y.toValue = @(l4c. y+= 62);
+    L4Y.springBounciness = 10;
+    L4Y.springSpeed = 10;
+    [_L4 pop_addAnimation:L4Y forKey:@"L4Y"];
+    
+    POPSpringAnimation * L5Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
+    L5Y.beginTime = CACurrentMediaTime() + 2.5;
+    L5Y.toValue = @(l5c. y+= 62);
+    L5Y.springBounciness = 10;
+    L5Y.springSpeed = 10;
+    [_L5 pop_addAnimation:L5Y forKey:@"L5Y"];
+    
+    POPSpringAnimation * L6Y = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionY];
+    L6Y.beginTime = CACurrentMediaTime() + 2.5;
+    L6Y.toValue = @(l6c. y+= 62);
+    L6Y.springBounciness = 10;
+    L6Y.springSpeed = 10;
+    [_L6 pop_addAnimation:L6Y forKey:@"L6Y"];
 }
-
-
 @end
